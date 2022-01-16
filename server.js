ba3b425e66cb944d946272b8a5f9122c393d6b33
David Roman <dmroman395@gmail.com>
2022-01-16 10:30:10 -0500

    Added dotenv package to use env var

--- server.js
+++ server.js
@@ -8,18 +8,6 @@ const app = express()
 
 app.use(cors())
 
-// app.get('/', (req, res) => {
-//     res.json('test')
-// })
-
-app.get('/coordinates', (req, res) => {
-    try {
-        const country = getCountryFromCoordinates(req.query.lat, req.query.lon)
-        console.log(country)
-       return res.send(country)
-    } catch(err) {
-        console.error(err)
-    } 
-})
+app.get('/coordinates', getCountryFromCoordinates)
 
 app.listen(4000, () => {console.log('App listening on port 4000...')})
